017556917c09079e37b252e67dc69f77af1a851b
Andres Rios <arios39@ucmerced.edu>
2019-05-17 14:12:17 -0700

    Add files via upload

---
+++ Lab51/App.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "App.h"
+
+App* singleton;
+
+void timer(int id){
+    // This will get called every 16 milliseconds after
+    // you call it once
+    
+    // If you want to manipulate the app in here
+    // do it through the singleton pointer
+    
+    glutTimerFunc(16, timer, id);
+}
+
+
+App::App(int argc, char** argv, int width, int height, const char* title): GlutApp(argc, argv, width, height, title){
+    
+    background = new TexRect("/Users/andresrios/Lab5/bg.png", -2, 1, 4, 2);
+    background2 = new TexRect("/Users/andresrios/Lab5/bg.png", 2, 1, 4, 2);
+    bobcat = new AnimatedRect("/Users/andresrios/Lab5/cat.png", 4, 2, 100, true, false, -0.04, -0.25, 0.8, 0.5);
+    bobcat->playLoop();
+    
+    currentX = background->getX(), newX = currentX, diff = currentX;
+}
+
+void App::draw() {
+    
+    background->draw(0.0);
+    background2->draw(0.0);
+    bobcat->draw(1.0);
+        
+    float modi = 0.04;
+    
+    newX -= modi;
+    diff += modi;
+    background->setX(newX);
+    background2->setX(0 - diff);
+    
+    if (!(diff < 2)) {
+        background->setX(-2.0);
+        background2->setX(0.0);
+        diff = -2.0;
+        newX = -2.0;
+    }
+}
+
+void App::keyDown(unsigned char key, float x, float y){
+    if (key == 27){
+        exit(0);
+    }
+}
+
+App::~App(){
+    std::cout << "Exiting..." << std::endl;
+}
---
+++ Lab51/App.h
@@ -0,0 +1,30 @@
+
+#ifndef App_h
+#define App_h
+
+#include "GlutApp.h"
+#include "AnimatedRect.h"
+
+
+class App: public GlutApp {
+    TexRect* background;
+    TexRect* background2;
+    AnimatedRect* bobcat;
+    
+    float currentX, newX, diff;
+    
+public:
+    
+    App(int argc, char** argv, int width, int height, const char* title);
+    
+    void draw();
+    
+    void keyDown(unsigned char key, float x, float y);
+    
+    ~App();
+    
+    friend void timer(int);
+    
+};
+
+#endif
